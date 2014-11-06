643f16b28c4e4d56b9715f6ad8d07df38c765f57
Apache <Apache@95.68.84.236>
2014-11-06 21:06:02 +0200

    ADD: Background image (fullscreen)

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -5,15 +5,14 @@
     </head>
 
     <body>
-                
 
         <!--Container--> 
         <div class="container my-main-container">
-            
+
             @if(Session::has('global'))
-                <p>{{ Session::get('global') }}</p>
+            <p>{{ Session::get('global') }}</p>
             @endif
-            
+
             <header class="row">
                 @include('includes.header')
             </header>
@@ -23,14 +22,13 @@
 
         </div>
 
-
         <!-- Scripts are placed here -->
         {{ HTML::script('js/jquery-2.1.1.js') }}
         {{ HTML::script('js/bootstrap.min.js') }}
         {{ HTML::script('js/site.js') }}
 
     </body>
-    
+
     <footer>
         @include('includes.footer')
     </footer>   
