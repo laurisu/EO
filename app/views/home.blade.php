3168505b5167a595e61d9b848b36ac09aa7ba837
laurisu <lauris.ulpe@gmail.com>
2014-11-23 09:39:11 +0200

    ADD: Pagination to products list

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -9,9 +9,12 @@
     @if(Auth::check())
         <p>Hello, {{ Auth::user()->username }}.</p>
     @else
+    
+    <div class="my-welocome-page center-block">
         <p>You are not signed in!</p>
         
         <a class="btn btn-lg btn-warning" href="{{ URL::route('sign-in') }}">Sign in</a>
+    </div>
         
     @endif  
 @stop
--- app/views/pages/products.blade.php
+++ app/views/pages/products.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 
-<div class="container-fluid my-main-container">
+<div class="container my-main-container">
     <table class="table table-condensed table-hover">
         <caption>Products list</caption>
         <thead>
