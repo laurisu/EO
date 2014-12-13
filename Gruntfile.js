06921d6052adb07c6ed58f245de95ff6b8dfbe16
laurisu <lauris.ulpe@gmail.com>
2014-12-13 12:13:00 +0200

    ADD: Grunt added

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: false, // flase makes minified version
+                    yuicompress: false, // bibliotēka, lai CSS padarītu mazāku
+                    optimization: 2, // 0 - priekš debug, 2 - sataisa css optimizētaāku
+                    sourceMap: true, // sourceMap faili paredzēti, lai varētu redzēt oriģinālos failus caur inspektoru
+                    sourceMapFilename: 'public/css/site.css.map', // kur grantam ielikt source map
+                    sourceMapURL: '/css/site.css.map', // 
+                    sourceMapRootpath: '/',
+                    outputSourceFiles: true  //  
+                },
+                files: {
+                    "public/css/site.css": "public/less/init.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['public/less/**/*.less'], // faili, kurus vērot, ** - visās mapēs
+                tasks: ['less'], // atrodot failus tiek pielietots uzdevums (task) "less"
+                options: {
+                    nospawn: true // true neizvadīs komadrindā tehnisko informāciju
+                }
+            }
+        }
+
+    });
+
+    // Load the plugin that provides the tasks.
+    grunt.loadNpmTasks('grunt-contrib-less'); // kompilē less failus uz css
+    grunt.loadNpmTasks('grunt-contrib-watch'); // vēro un pārkompilē less failu izmaiņas
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
+
