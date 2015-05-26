6c4539507e27a8df4d3db6d8afc37104bfc10986
laurisu <lauris.ulpe@gmail.com>
2015-05-26 19:50:34 +0300

    Ajax search removed

--- public/js/site.js
+++ public/js/site.js
@@ -17,25 +17,6 @@ $(function () {
         });
     });
 
-    $('.js-ajax-search').on('submit', function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: $(this).attr("action"), // paņem linku no formas attr action un sūta uz to formas datus 
-            type: $(this).attr("method"), // paņem metodi no formas
-            data: $(this).serializeArray(), // sagatavo fromas datus sūtīšanai ar AJAX
-            beforeSend: function () {
-                $(".js-ajax-products").html('<div class="my-loader-container"><div class="my-loader"></div></div>');
-                $(".js-ajax-pages").html('');
-            },
-            success: function (result) {
-                $(".js-ajax-products").html($(result).find('.js-products').html());
-                applyResponsiveTable('.table-responsive');
-
-                $(".js-ajax-pages").html($(result).find('.js-pages').html());
-            }
-        });
-    });
-
     /**
      *  Responsive tables plugin
      */
